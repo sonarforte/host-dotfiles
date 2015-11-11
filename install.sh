f96bc219ce02368b6faac84e83682fbd2cd0c3a6
sonarforte <sonarforte@protonmail.com>
2015-11-10 22:48:57 -0500

    Added link: no more testing on home machine

--- install.sh
+++ install.sh
@@ -12,6 +12,19 @@ for file in "${runcoms[@]}"; do
     echo $file
     dotname=$(basename "$file")
     echo $dotname
-
+    # Test if diff returns a string: "the files differ..." 
+    dotfile="$HOME/.$dotname" 
+    echo $dotfile
+    # Copy old dotfiles to runcoms_old directory
+    if [ -f $dotfile ]; then
+        if [ -n "`diff "$dotfile" "$file"`" ]; then 
+            echo "Copying old dotfiles..."
+            date=$(date +"%Y-%m-%d:%k:%M")
+            cat "$dotfile" > "./runcoms_old/$dotname-$date" 
+    
+        fi
+    fi
+    # Link new rc file to home directory
+    ln -s "$file" "$dotfile" 
 
 done
